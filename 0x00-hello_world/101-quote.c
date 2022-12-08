471499f01a7974e8f0d23d0e5fd96df68932da3a
NessBironga <nessbironga@gmail.com>
2022-12-08 15:03:58 -0800

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
   */
 int main(void)
 {
-	fcout(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", 59); 
+	fgets(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", 59); 
 	return (1);
 }
