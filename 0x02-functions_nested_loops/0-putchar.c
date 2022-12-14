91ca2d6828ec5e8e672983b9240b7bcfc0d0af79
NessBironga <nessbironga@gmail.com>
2022-12-14 15:09:19 -0800

    task 0-1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main -program that prints _putchar
+ * main - print _putchar
  *
  * Return: 0
  */
-int mai
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
